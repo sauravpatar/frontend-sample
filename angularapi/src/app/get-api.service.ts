ec4e238a310d4e047045a5825af66cf5bd441503
aniket <aniketsinhanni@gmail.com>
2020-07-22 17:38:12 +0530

    this is newerversion

---
+++ angularapi/src/app/get-api.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+@Injectable({
+  providedIn: 'root'
+})
+export class GetApiService {
+
+  constructor( 
+    private http:HttpClient
+  ) { }
+
+  apicall()
+  {
+    return this.http.get('http://127.0.0.1:5000/')
+  }
+}
+
